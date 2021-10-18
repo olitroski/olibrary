7866b67384d4b612bf1221300845cf7029682553
Oliver Rojas <oliver.rojas.b@gmail.com>
2021-10-18 16:09:28 -0300

    arreglo dt
    quedo con notas y warnings... revisar

--- R/otable.r
+++ R/otable.r
@@ -10,7 +10,7 @@
 #' @return Devuelve una dataframe si 1 var o lista con las 4 tablas y el test si contingencia. Seg?n clip se pasa al clipboard.
 #'
 #' @export
-#' 
+#'
 #' @examples
 #' # otable(rvar = "cyl", data = mtcars)
 #' # otable(rvar = "cyl", cvar = "am", data = mtcars, clip = 0)
@@ -20,196 +20,134 @@
 #' @importFrom utils write.table
 #' @importFrom dplyr select
 #' @importFrom dplyr mutate
-
-
-
+#' @importFrom janitor adorn_totals
+#' @import data.table
 
 otable <- function(rvar = NULL, cvar = NULL, data = NULL, clip = 0){
-     # Los bindings
-     Freq <- total <- NULL
+    # Los bindings
+    Freq <- total <- NULL
 
-     # Si es que si lo hay row var (1 variable)
-     if (class(cvar) == "NULL"){
-          # Tablas
-          df <- paste("table(data$", rvar, ")", sep = "")
-          df <- eval(parse(text=df))
+    # Si es que si lo hay row var (1 variable)
+    if (class(cvar) == "NULL"){
+        # Tablas
+        df <- paste("table(data$", rvar, ")", sep = "")
+        df <- eval(parse(text=df))
 
-          freq <- data.frame(df)
-          names(freq) <- c(rvar, "freq")
-          margin <- data.frame("total", margin.table(df))
-          names(margin) <- names(freq)
-          freq <- rbind(freq, margin)
+        freq <- data.frame(df)
+        names(freq) <- c(rvar, "freq")
+        margin <- data.frame("total", margin.table(df))
+        names(margin) <- names(freq)
+        freq <- rbind(freq, margin)
 
-          pct <- data.frame(prop.table(df))
-          names(pct) <- c(rvar, "pct")
-          margin <- data.frame("total", margin.table(prop.table(df)))
-          names(margin) <- names(pct)
-          pct <- rbind(pct, margin)
-          pct <- select(pct, pct)
+        pct <- data.frame(prop.table(df))
+        names(pct) <- c(rvar, "pct")
+        margin <- data.frame("total", margin.table(prop.table(df)))
+        names(margin) <- names(pct)
+        pct <- rbind(pct, margin)
+        pct <- select(pct, pct)
 
-          df <- cbind(freq, pct)
-          df <- mutate(df, pct = round(pct, 3))
+        df <- cbind(freq, pct)
+        df <- mutate(df, pct = round(pct, 3))
 
-          # Resultado
-          if (clip == 1){
-               write.table(df, "clipboard-128", sep="\t", row.names=FALSE)
-          }
-          return(df)
-
-
-     # tablas de 2x2
-     } else {
-          # Primero las tablas
-          df <- paste("table(data$", rvar, ", data$", cvar, ")", sep = "")
-          df <- eval(parse(text=df))
-
-          # Tabla de frecuencias
-          freq <- data.frame(df)
-          names(freq) <- c(rvar, "time", cvar)
-          freq <- reshape(freq, timevar = "time", idvar = rvar, direction = "wide")
-
-          cols <- colSums(freq[,(2:dim(freq)[2])])   ## Ta weno pero no aguanta largos nombres
-          cols <- data.frame(t(cols))
-          cols <- cbind(data.frame("total"), cols)
-          names(cols) <- names(freq)
-          freq <- rbind(freq, cols)
-
-          rows <- rowSums(freq[,(2:dim(freq)[2])])
-          rows <- data.frame(total = rows)
-          freq <- cbind(freq, rows)
+        # Resultado
+        if (clip == 1){
+            write.table(df, "clipboard-128", sep="\t", row.names=FALSE)
+        }
+        return(df)
 
 
-          # Porcentaje por columnas
-          pctcol <- prop.table(df, margin = 2)
-          pctcol <- data.frame(pctcol)
-          names(pctcol) <- c(rvar, "time", cvar)
-          pctcol <- reshape(pctcol, timevar = "time", idvar = rvar, direction = "wide")
+        # tablas de 2x2
+    } else {
+        # Es razonable usar loops porque nunca voy a tener una tabla muy grande
+        # Transformar a data.table
+        if (length(grep("data.table", class(data))) == 0){
+            setDT(data)
+        }
 
-          rows <- paste("table(data$", rvar, ")", sep = "")
-          rows <- eval(parse(text=rows))
-          rows <- prop.table(rows)
-          rows <- data.frame(rows)
-          rows <- select(rows, total = Freq)
-          pctcol <- cbind(pctcol, rows)
-
-          cols <- colSums(pctcol[,(2:dim(pctcol)[2])])
-          cols <- data.frame(t(cols))
-          cols <- cbind(data.frame("total"), cols)
-          names(cols) <- names(pctcol)
-          pctcol <- rbind(pctcol, cols)
-
-          col1 <- select(pctcol, 1)
-          pctcol <- select(pctcol, -1)
-          pctcol <- sapply(pctcol, function(x) round(x, 3))
-          pctcol <- cbind(col1, data.frame(pctcol))
+        # Tabla madre
+        dt <- data[, .N, by = .(get(rvar), get(cvar))]
+        names(dt) <- c(rvar, cvar, "N")
+        dt <- reshape(dt, timevar = cvar, idvar = rvar, direction = "wide")
 
 
-          # Porcentaje por filas
-          pctrow <- prop.table(df, margin = 1)
-          pctrow <- data.frame(pctrow)
-          names(pctrow) <- c(rvar, "time", cvar)
-          pctrow <- reshape(pctrow, timevar = "time", idvar = rvar, direction= "wide")
+        # Tabla de frecuencias  ---------------
+        freq <- dt
+        freq <- adorn_totals(freq, 'row')
+        freq <- adorn_totals(freq, 'col')
 
-          cols <- paste("table(data$", cvar, ")", sep = "")
-          cols <- eval(parse(text=cols))
-          cols <- prop.table(cols)
-          cols <- data.frame(cols)
-          cols <- select(cols, Freq)
-          cols <- cbind(data.frame("total"), data.frame(t(cols)))
-          names(cols) <- names(pctrow)
-          pctrow <- rbind(pctrow, cols)
-
-          rows <- rowSums(pctrow[,(2:dim(pctrow)[2])])
-          rows <- data.frame(total = rows)
-          pctrow <- cbind(pctrow, rows)
-
-          col1 <- select(pctrow, 1)
-          pctrow <- select(pctrow, -1)
-          pctrow <- sapply(pctrow, function(x) round(x, 3))
-          pctrow <- cbind(col1, data.frame(pctrow))
-          pctrow <- mutate(pctrow, total = sprintf("%.3f", total))
-          row.names(pctrow) <- NULL
+        # Porcentaje por columnas -------------
+        pctcol <- as.data.frame(dt)
+        pctcol <- adorn_totals(pctcol, "col")
+        for (j in 2:ncol(pctcol)){
+            pctcol[, j] <- pctcol[, j]/sum(pctcol[, j], na.rm = TRUE)
+        }
+        pctcol <- adorn_totals(pctcol, "row")
 
 
-          # Porcentaje por celdas
-          pctcell <- prop.table(df)
-          pctcell <- data.frame(pctcell)
-          names(pctcell) <- c(rvar, "time", cvar)
-          pctcell <- reshape(pctcell, timevar = "time", idvar = rvar, direction = "wide")
+        # Porcentaje por filas ---------------
+        pctrow <- as.data.frame(dt)
+        pctrow <- adorn_totals(pctrow, "row")
+        for (i in 1:nrow(pctrow)){
+            pctrow[i, 2:ncol(pctrow)] <- pctrow[i, 2:ncol(pctrow)] / sum(pctrow[i, 2:ncol(pctrow)], na.rm = TRUE)
+        }
+        pctrow <- adorn_totals(pctrow, "col")
 
-          rows <- paste("table(data$", rvar, ")", sep = "")
-          rows <- eval(parse(text=rows))
-          rows <- prop.table(rows)
-          rows <- data.frame(rows)
-          rows <- select(rows, total = Freq)
-          pctcell <- cbind(pctcell, rows)
+        # Porcentaje por celdas --------------
+        pctcell <- as.data.frame(dt)
+        N <- sum(pctcell[, 2:ncol(pctcell)], na.rm = TRUE)
+        pctcell <- adorn_totals(pctcell, "row")
+        pctcell <- adorn_totals(pctcell, "col")
 
-          cols <- paste("table(data$", cvar, ")", sep = "")
-          cols <- eval(parse(text=cols))
-          cols <- prop.table(cols)
-          cols <- data.frame(cols)
-          cols <- select(cols, Freq)
-          cols <- cbind(data.frame("total"), data.frame(t(cols)))
-          cols <- mutate(cols, total = 1)
-          names(cols) <- names(pctcell)
-          pctcell <- rbind(pctcell, cols)
+        for (i in 1:nrow(pctcell)){
+            for (j in 2:ncol(pctcell)){
+                pctcell[i, j] <- pctcell[i, j]/N
+            }
+        }
 
-          col1 <- select(pctcell, 1)
-          pctcell <- select(pctcell, -1)
-          pctcell <- sapply(pctcell, function(x) round(x, 3))
-          pctcell <- cbind(col1, data.frame(pctcell))
-          row.names(pctcell) <- NULL
+        # Test de independencia ---------------
+        chi <- summary(table(data[[rvar]], data[[cvar]]))
+        chi <- data.frame(table = paste(rvar, "*", cvar),
+                          ChiSq = sprintf("%.3f", chi$p.value))
 
 
-          # Test de independencia   .)
-#           F <- fisher.test(df)
-#           F <- F$p.value
+        # Resultado
+        lista <- list(freq=freq, row=pctrow, col=pctcol, cell=pctcell, pvalue = chi)
 
-          chi <- summary(df)
-          chi <- data.frame(table = paste(rvar, "*", cvar),
-               ChiSq = sprintf("%.3f", chi$p.value))
-#              ChiSq = sprintf("%.3f", chi$p.value), Exact = sprintf("%.3f", F))
+        ## Resultados seg?n tipo de clip
+        if (clip == 0){
+            write.table(lista, "clipboard-128", sep="\t", row.names=FALSE)
+            return(lista)
 
+        } else if (clip == 1){
+            tab <- lista$freq
+            write.table(tab, "clipboard-128", sep="\t", row.names=FALSE)
+            return(tab)
 
-          # Resultado
-          lista <- list(freq=freq, row=pctrow, col=pctcol, cell=pctcell, pvalue = chi)
+        } else if (clip == 2){
+            tab <- lista$row
+            write.table(tab, "clipboard-128", sep="\t", row.names=FALSE)
+            return(tab)
 
+        } else if (clip == 3){
+            tab <- lista$col
+            write.table(tab, "clipboard-128", sep="\t", row.names=FALSE)
+            return(tab)
 
-          ## Resultados seg?n tipo de clip
-          if (clip == 0){
-               write.table(lista, "clipboard-128", sep="\t", row.names=FALSE)
-               return(lista)
+        } else if (clip == 4){
+            tab <- lista$cell
+            write.table(tab, "clipboard-128", sep="\t", row.names=FALSE)
+            return(tab)
 
-          } else if (clip == 1){
-               tab <- lista$freq
-               write.table(tab, "clipboard-128", sep="\t", row.names=FALSE)
-               return(tab)
+        } else if (clip == 5){
+            tab <- lista$pvalue
+            write.table(tab, "clipboard-128", sep="\t", row.names=FALSE)
+            return(tab)
 
-          } else if (clip == 2){
-               tab <- lista$row
-               write.table(tab, "clipboard-128", sep="\t", row.names=FALSE)
-               return(tab)
+        } else {
+            stop()
+        }
 
-          } else if (clip == 3){
-               tab <- lista$col
-               write.table(tab, "clipboard-128", sep="\t", row.names=FALSE)
-               return(tab)
-
-          } else if (clip == 4){
-               tab <- lista$cell
-               write.table(tab, "clipboard-128", sep="\t", row.names=FALSE)
-               return(tab)
-
-          } else if (clip == 5){
-               tab <- lista$pvalue
-               write.table(tab, "clipboard-128", sep="\t", row.names=FALSE)
-               return(tab)
-
-          } else {
-               stop()
-          }
-
-     }
+    }
 
 }
 
